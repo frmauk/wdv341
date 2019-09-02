850e2f70826d7e93e24f085325342f6220a22b8c
frmauk <40443975+frmauk@users.noreply.github.com>
2019-09-02 15:29:43 -0500

    Update testGit.php

--- testGit.php
+++ testGit.php
@@ -1,2 +1,19 @@
 <h3>Wdv341 intro to php</h3>
 <h2>Faith Mauk</h2>
+<h4>What is Version Control Software</h4>
+<p> Version Control Software are tools that allow you to view changes to your code over time. This allows teams to work together, and when they run into issues they can look back through previous versions of source code and see where mistakes may have been made.  </p>
+
+<h4>Define git add</h4>
+<p>git add tells git you would like to add updates to a particular file in the next commit. It doesn't actually change the code until you commit it. </p>
+
+<h4>Define git Commit</h4>
+<p>Git commit is used to commit files that are in the local repo, before using push</p>
+
+<h4> define git push</h4>
+<p>git push is used to upload content from the local repo to the remote repo</p>
+
+<h4>define git pull</h4>
+<p>Git pull is used to fetch content from the remote repo, and immediately update the local repo</p>
+
+<h4>Define git clone</h4>
+<p>Git clone is used to fetch a remote repo, and create an exact copy of the target repo</p>

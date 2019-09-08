1eae689db15c5021ff3d006c77121e6a92083de0
frmauk <40443975+frmauk@users.noreply.github.com>
2019-09-08 15:05:44 -0500

    Add files via upload

---
+++ phpbasic.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP basics</title>
+</head>
+
+<body>
+			<?php 
+			$yourName = "Faith Mauk"; 
+			// 1. This variable contains my full name
+			
+			echo "<h1>Assignment: PHP Basics</h1>";
+			// 2. This will display the assignment name, putting it in an h1 element
+		?>
+</body>
+</html>

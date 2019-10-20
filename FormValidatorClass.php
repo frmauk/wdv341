d5c233bf01152b1d4de03c08c1a11866ed7a1239
frmauk <40443975+frmauk@users.noreply.github.com>
2019-10-20 15:55:20 -0500

    Update FormValidatorClass.php

--- FormValidatorClass.php
+++ FormValidatorClass.php
@@ -7,44 +7,44 @@
 	
 
 		
-		public function filterSpecialCharacters($inFieldValue) {
+public function filterSpecialCharacters($inFieldValue) {
 			$inFieldValue = htmlspecialchars($inFieldValue);
 			$inFieldValue = filter_var($inFieldValue, FILTER_SANITIZE_SPECIAL_CHARS);
 			return $inFieldValue;
 		}
 		
-		//check if value is not spaces, "", 0, 0.0, "0", null, false, array()
-		public function cannotBeEmpty($inFieldValue) {
+		//check if value is not empty 
+public function cannotBeEmpty($inFieldValue) {
 			$inFieldValue = trim($inFieldValue);
 			if( empty($inFieldValue) ) {
 				return true;
 			}
 		}
 		
-		public function cannotBeNullOrUndefined($inFieldValue) {
+public function cannotBeNullOrUndefined($inFieldValue) {
 			if(preg_match("/undefined|null/i", $inFieldValue) ) {
 				return true;
 			}
 		}
-		public function isNumber($inFieldValue) {
+public function isNumber($inFieldValue) {
 			if( !$inFieldValue == "" && !is_numeric($inFieldValue) ) {
 				return true;
 			}
 		}
 		
-		public function validatePhoneCharacters($inFieldValue) {
+public function validatePhoneCharacters($inFieldValue) {
 			if( preg_match("/[-\(\)]/", $inFieldValue) ) {
 				return true;
 			}
 		}
 		
-		public function validatePhoneLength($inFieldValue) {
+public function validatePhoneLength($inFieldValue) {
 			if( strlen($inFieldValue) > 0 && strlen($inFieldValue) < 10 ) {
 				return true;
 			}
 		}
 	
-		public function validateEmail($inEmail) {			
+public function validateEmail($inEmail) {			
 			$inEmail = filter_var($inEmail, FILTER_SANITIZE_EMAIL);	//clean entered value
 			$inEmail = filter_var($inEmail, FILTER_VALIDATE_EMAIL);	//validate format
 			if(!$inEmail) {
@@ -52,13 +52,13 @@
 			}
 		}
 		
-		public function selectionRequired($inFieldValue) {
+public function selectionRequired($inFieldValue) {
 			if(!$inFieldValue) {
 				return true;
 			}
 		}
 		
-		public function characterLengthLessThan200($inFieldValue) {
+public function characterLengthLessThan200($inFieldValue) {
 			if( strlen($inFieldValue) > 200 ) {
 				return true;
 			}

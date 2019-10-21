d08148f4e2172a6deaa7d5c9cafc207a01beb6d1
frmauk <40443975+frmauk@users.noreply.github.com>
2019-10-21 10:35:01 -0500

    Add files via upload

--- FormValidatorClass.php
+++ FormValidatorClass.php
@@ -7,60 +7,63 @@
 	
 
 		
-public function filterSpecialCharacters($inFieldValue) {
+		public function filterSpecialCharacters($inFieldValue) {
 			$inFieldValue = htmlspecialchars($inFieldValue);
 			$inFieldValue = filter_var($inFieldValue, FILTER_SANITIZE_SPECIAL_CHARS);
 			return $inFieldValue;
 		}
 		
-		//check if value is not empty 
-public function cannotBeEmpty($inFieldValue) {
+		//check if value is not spaces, "", 0, 0.0, "0", null, false, array()
+		public function cannotBeEmpty($inFieldValue) {
 			$inFieldValue = trim($inFieldValue);
 			if( empty($inFieldValue) ) {
-				return true;
+				return "false";
 			}
+			else{ return "true";}
 		}
 		
-public function cannotBeNullOrUndefined($inFieldValue) {
+		public function cannotBeNullOrUndefined($inFieldValue) {
 			if(preg_match("/undefined|null/i", $inFieldValue) ) {
-				return true;
+				return "false";
 			}
 		}
-public function isNumber($inFieldValue) {
+		public function isNumber($inFieldValue) {
 			if( !$inFieldValue == "" && !is_numeric($inFieldValue) ) {
-				return true;
+				return "false";
 			}
+			else{return "true";}
 		}
 		
-public function validatePhoneCharacters($inFieldValue) {
+		public function validatePhoneCharacters($inFieldValue) {
 			if( preg_match("/[-\(\)]/", $inFieldValue) ) {
-				return true;
+				return "false";
 			}
+			else {return "true";}
 		}
 		
-public function validatePhoneLength($inFieldValue) {
+		public function validatePhoneLength($inFieldValue) {
 			if( strlen($inFieldValue) > 0 && strlen($inFieldValue) < 10 ) {
-				return true;
+				return "false";
 			}
 		}
 	
-public function validateEmail($inEmail) {			
+		public function validateEmail($inEmail) {			
 			$inEmail = filter_var($inEmail, FILTER_SANITIZE_EMAIL);	//clean entered value
 			$inEmail = filter_var($inEmail, FILTER_VALIDATE_EMAIL);	//validate format
 			if(!$inEmail) {
-				return true;
+				return "false";
 			}
 		}
 		
-public function selectionRequired($inFieldValue) {
+		public function selectionRequired($inFieldValue) {
 			if(!$inFieldValue) {
-				return true;
+				return "false";
 			}
 		}
 		
-public function characterLengthLessThan200($inFieldValue) {
+		public function characterLengthLessThan200($inFieldValue) {
 			if( strlen($inFieldValue) > 200 ) {
-				return true;
+				return "false";
 			}
 		}
 	}
